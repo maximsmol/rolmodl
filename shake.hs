08e0e22499d3c998e2d91edef4d651f0cfdeab57
Maksim Smolin <maximsmol@gmail.com>
2018-01-17 10:03:06 -0800

    add basic pixelfmt

--- shake.hs
+++ shake.hs
@@ -56,6 +56,7 @@ main = shakeArgs shakeOptions' $ do
           path_libout</>"Win"<.>"o",
           path_libout</>"Ren"<.>"o",
           path_libout</>"Base"<.>"o",
+          path_libout</>"PixelFmt"<.>"o",
           path_tstout</>"main"<.>"o"
           ]
     need srcs
