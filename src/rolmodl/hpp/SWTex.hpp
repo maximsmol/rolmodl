57e1427d5dc8c56479ae12ea8c4cfb607e3807b8
Maksim Smolin <1472826+maximsmol@users.noreply.github.com>
2019-03-19 13:40:19 -0700

    add basic SWTex (SDL_Surface) support

---
+++ src/rolmodl/hpp/SWTex.hpp
@@ -0,0 +1,123 @@
+#pragma once
+#include <cstdint>
+
+#include <SDL.h>
+
+namespace rolmodl {
+  class SWTex_Base;
+  class SWTex;
+  class SWTex_RLE;
+  class SWTex_RLELock;
+  class Win_SW;
+}
+
+#include "Base.hpp"
+#include "PixelFmt.hpp"
+#include "Geom.hpp"
+#include "Ren.hpp"
+
+namespace rolmodl {
+  class SWTex_Base {
+    public:
+      SWTex_Base(const geom::Size s, const pixelfmt::Id fmt);
+      ~SWTex_Base() noexcept;
+
+      SWTex_Base(const SWTex_Base& that) = delete;
+      SWTex_Base(SWTex_Base&& that) noexcept;
+
+      SWTex_Base& operator=(const SWTex_Base& that) = delete;
+      SWTex_Base& operator=(SWTex_Base&& that) noexcept;
+
+      friend void swap(SWTex_Base& a, SWTex_Base& b) noexcept;
+
+      pixelfmt::Id fmtId() noexcept;
+      pixelfmt::Id fmtId() const noexcept;
+
+      SDL_PixelFormat& fmt() noexcept;
+      SDL_PixelFormat& fmt() const noexcept;
+
+      unsigned int pitch() noexcept;
+      unsigned int pitch() const noexcept;
+
+      int w() noexcept;
+      int w() const noexcept;
+      int h() noexcept;
+      int h() const noexcept;
+
+      bool mustLock() noexcept;
+      bool mustLock() const noexcept;
+
+      SDL_Surface* unsafeRaw() noexcept;
+      const SDL_Surface* unsafeRaw() const noexcept;
+
+      uint32_t& unsafePoint(const geom::Pos p) noexcept;
+      const uint32_t& unsafePoint(const geom::Pos p) const noexcept;
+
+      void* unsafeRawPixels() noexcept;
+      const void* unsafeRawPixels() const noexcept;
+
+    protected:
+      SWTex_Base();
+
+      friend class Win_SW;
+      friend class SWTex;
+      friend class SWTex_RLE;
+      SDL_Surface* h_;
+
+      friend class SWTex_RLELock;
+      SWTex_Base& drawPoint(const RGBA c, const geom::Pos p) noexcept;
+      RGBA getPoint(const geom::Pos p) const noexcept;
+  };
+
+  class SWTex : public SWTex_Base {
+    public:
+      SWTex(const geom::Size s, const pixelfmt::Id fmt);
+      explicit SWTex(SWTex_RLE&& rle) noexcept;
+
+      SWTex& drawPoint(const RGBA c, const geom::Pos p) noexcept;
+      RGBA getPoint(const geom::Pos p) const noexcept;
+
+    private:
+      friend class Win_SW;
+      SWTex();
+  };
+
+  class SWTex_RLE : public SWTex_Base {
+    public:
+      SWTex_RLE(const geom::Size s, const pixelfmt::Id fmt);
+      explicit SWTex_RLE(SWTex&& nonRle) noexcept;
+  };
+
+  class SWTex_RLELock {
+    public:
+      explicit SWTex_RLELock(SWTex_RLE& tex);
+      ~SWTex_RLELock() noexcept;
+
+      SWTex_RLELock(const SWTex_RLELock& that) = delete;
+      SWTex_RLELock(SWTex_RLELock&& that) noexcept;
+
+      SWTex_RLELock& operator=(const SWTex_RLELock& that) = delete;
+      SWTex_RLELock& operator=(SWTex_RLELock&& that) noexcept;
+
+      friend void swap(SWTex_RLELock& a, SWTex_RLELock& b) noexcept;
+
+      SWTex_RLELock& drawPoint(const RGBA c, const geom::Pos p) noexcept;
+      RGBA getPoint(const geom::Pos p) const noexcept;
+
+    private:
+      SWTex_RLELock() noexcept;
+
+      SWTex_RLE* h_;
+  };
+
+  class Win_SW : public Win_Base {
+    public:
+      using Win_Base::Win_Base;
+      ~Win_SW() noexcept; // todo: we could rely on the SDL_Surface do not free flag instead
+
+      SWTex& swtex();
+
+    private:
+      SWTex tex;
+  };
+}
---
+++ src/test/main_swtex.cpp
@@ -0,0 +1,53 @@
+#include <cstdio>
+#include <optional>
+#include <variant>
+#include <functional>
+
+#include "Rolmodl.hpp"
+
+using namespace std;
+using namespace rolmodl;
+using namespace geom;
+
+template<class T, class... Types>
+void varact(function<void(T)> f, variant<Types...> v) {
+  if (!holds_alternative<T>(v))
+    return;
+
+  f(get<T>(v));
+}
+
+template<class R, class T, class... Types>
+optional<R> varact(function<R(T)> f, variant<Types...> v) {
+  if (!holds_alternative<T>(v))
+    return nullopt;
+
+  return f(get<T>(v));
+}
+
+int main() {
+  SDL_Init(SDL_INIT_EVERYTHING);
+
+  Size size{800, 600};
+  Win_SW w("test", size);
+  SWTex& screenTex = w.swtex();
+
+  bool running = true;
+  while (running) {
+    while (true) {
+      optional polled = event::poll();
+      if (!polled)
+        break;
+
+      Event e = polled.value();
+      varact<event::Quit>([&](event::Quit x) {
+        printf("%6d: Quit\n", x.timestamp);
+        running = false;
+      }, e);
+    }
+
+    SDL_Delay(1000/24);
+  }
+
+  return 0;
+}
